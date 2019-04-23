e3e3f201210613714de02e537e993bfc5250165c
sergey <svolchenkov@gmail.com>
2019-04-23 13:35:00 -0500

    cut db

--- src/main/java/com/capgemini/repositories/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.capgemini.entities.AccountEntity;
-
-public interface AccountRepository extends CrudRepository<AccountEntity, Integer> {
-
-	}
